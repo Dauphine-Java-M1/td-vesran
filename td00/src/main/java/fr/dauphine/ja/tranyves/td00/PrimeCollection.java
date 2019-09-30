106b1f037dd5d5d87278020fc767eb8ae7c8ff59
tranyv16 <tranyv16@b028-17.crio.dauphine.fr>
2019-09-30 14:21:46 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/tranyves/td00/PrimeCollection.java
@@ -0,0 +1,58 @@
+package fr.dauphine.ja.tranyves.td00;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class PrimeCollection {
+	
+	private List<Integer> numbers;
+	
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>();
+	}
+	
+	public void initRandom(int n, int m) {
+		Random rand = new Random();
+		
+		for (int i = 0; i < n; i++) {
+			this.numbers.add(rand.nextInt(m));
+		}
+	}
+	
+	public boolean isPrime(Integer p) {
+		if (p <= 1) {
+			return false;
+		} 
+		for (int i = 2; i < Math.sqrt(p); i++) {
+			if (p % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public void printPrime() {
+		for (Integer nb : this.numbers) {
+			if (this.isPrime(nb)) {
+				System.out.println(nb);
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return this.numbers.toString();
+	}
+	
+    public static void main( String[] args )
+    {
+        PrimeCollection pc = new PrimeCollection();
+        pc.initRandom(100, 2000);
+        System.out.println(pc);
+        
+        pc.printPrime();
+        System.out.println(pc.isPrime(0));
+    }
+}
