b07904f94b937e01b5512c4b6652f22ab48c0076
tranyv16 <tranyv16@b028-17.crio.dauphine.fr>
2019-09-30 15:16:38 +0200

    added test template

---
+++ td00/src/test/java/fr/dauphine/ja/tranyves/td00/PrimeCollectionTest.java
@@ -0,0 +1,40 @@
+package fr.dauphine.ja.tranyves.td00;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+// import static junit.framework.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeCollectionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrimeCollectionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrimeCollectionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
