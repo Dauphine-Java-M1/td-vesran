296b3b4077ac81fdd34f494774636720dd489ee6
vesran <tran.yves@gmail.com>
2019-10-07 15:12:40 +0200

    polygonalChain constructor done

---
+++ shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/PolygonalChain.java
@@ -0,0 +1,12 @@
+package fr.dauphine.ja.tranyves.shapes;
+
+public class PolygonalChain {
+
+    private Point [] points;
+
+    public PolygonalChain(int size) {
+        this.points = new Point[size];
+    }
+
+    
+}
