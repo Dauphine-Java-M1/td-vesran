073a6404bd7e6cf672cb9e8d3aa4bbf2c623ffd2
vesran <tran.yves@gmail.com>
2019-10-07 12:38:48 +0200

    added a static variable to store the number of points

--- shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/Point.java
@@ -1,17 +1,24 @@
 package fr.dauphine.ja.tranyves.shapes;
 
+
 public class Point {
 
+    public static int numberOfPoints;
     private int x;
     private int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+
+        // Increase the number of points
+        Point.numberOfPoints++;
     }
 
     public static void main(String [] args) {
-        Point p = new Point(1, 1);
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(8, 9);
+        System.out.println(Point.numberOfPoints);
     }
 
 }
