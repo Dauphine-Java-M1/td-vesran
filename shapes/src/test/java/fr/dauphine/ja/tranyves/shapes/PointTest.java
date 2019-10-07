246bb51836255af17a7b2e8932412b123045a7ff
vesran <tran.yves@gmail.com>
2019-10-07 16:23:32 +0200

    rename file to Pointtest in test section

---
+++ shapes/src/test/java/fr/dauphine/ja/tranyves/shapes/PointTest.java
@@ -0,0 +1,37 @@
+package fr.dauphine.ja.tranyves.shapes;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PointTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PointTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PointTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue( true );
+    }
+}
