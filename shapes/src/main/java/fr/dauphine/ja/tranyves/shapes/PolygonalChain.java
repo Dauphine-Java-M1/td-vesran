04056351d9cbc26cd4f94413eb93d90e32a9d602
vesran <tran.yves@gmail.com>
2019-10-07 15:22:36 +0200

    wrote toString + add methods

--- shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/PolygonalChain.java
+++ shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/PolygonalChain.java
@@ -8,5 +8,39 @@ public class PolygonalChain {
         this.points = new Point[size];
     }
 
-    
+    public Point[] getPoints() {
+        return this.points;
+    }
+
+    public void add(Point p) {
+        for (int i = 0 ; i < this.points.length ; i++) {
+            if (this.points[i] == null) {
+                this.points[i] = p;
+                return;
+            }
+        }
+        System.out.println("The array of points is full !");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder strb = new StringBuilder();
+        strb.append("[");
+
+        for (Point p : this.points) {
+            strb.append(p + " ");
+        }
+
+        return strb.toString();
+    }
+
+    public static void main(String [] args) {
+        PolygonalChain pc = new PolygonalChain(10);
+
+        for (int i = 0 ; i < 13 ; i ++) {
+            pc.add(new Point(1, 2));
+        }
+
+        System.out.println(pc);
+    }
 }
