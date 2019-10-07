4f9edcb7304ec1f40f9595a969a84a30581872be
vesran <tran.yves@gmail.com>
2019-10-07 16:22:02 +0200

    added TestPoint class with main

---
+++ shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/TestPoint.java
@@ -0,0 +1,9 @@
+package fr.dauphine.ja.tranyves.shapes;
+
+public class TestPoint {
+
+    public static void main(String args) {
+//        Point p = new Point();
+//        System.out.println(p.x+" "+p.y)
+    }
+}
