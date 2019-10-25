f69cf749d5621b240278280a941834ea4019a78a
vesran <tran.yves@gmail.com>
2019-10-25 13:07:18 +0200

    wrote main for MyDisplay

---
+++ shapes/src/main/java/fr/dauphine/ja/tranyves/shapes/view/MyDisplay.java
@@ -0,0 +1,18 @@
+package fr.dauphine.ja.tranyves.shapes.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyDisplay extends JPanel {
+
+    public static void main(String [] args) {
+        JFrame frame = new JFrame("Java avancé - Graphic Display");
+
+        frame.setSize(new Dimension(500, 500));
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyDisplay d = new MyDisplay();
+        frame.add(d);
+    }
+}
