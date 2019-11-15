8384d14b72c365f4a1c66b2c592d369fb443b03f
vesran <tran.yves@gmail.com>
2019-11-15 13:35:03 +0100

    functional iterator from panel1 with anonymous class

--- iterable/src/main/java/fr/dauphine/ja/tranyves/iterable/Panel.java
+++ iterable/src/main/java/fr/dauphine/ja/tranyves/iterable/Panel.java
@@ -4,8 +4,31 @@ import java.util.Iterator;
 
 public class Panel {
 
-    public static Iterator<Integer> panel1(int firstValue, int lastValue) {
+    public static Iterator<Integer> panel1Old(int firstValue, int lastValue) {
         return new PanelIterator(firstValue, lastValue);
+    }
+
+    public static Iterator<Integer> panel1(final int firstValue, final int lastValue) {
+
+        return new Iterator<Integer>() {
+
+            private int currentValue = firstValue;
+
+            @Override
+            public boolean hasNext() {
+                return currentValue <= lastValue;
+            }
+
+            @Override
+            public Integer next() {
+                return currentValue++;
+            }
+
+            @Override
+            public void remove() {
+                // Do nothing
+            }
+        };
     }
 
     public static void main(String [] args) {
@@ -15,5 +38,6 @@ public class Panel {
         for (; it.hasNext(); ){
             System.out.println(it.next());
         }
+
     }
 }
