186c8016e4e0d41ff06aab4643b5e5c1ba8b0c56
vesran <tran.yves@gmail.com>
2019-11-15 14:04:27 +0100

    implemented panel2 : panel works in a for-each

--- iterable/src/main/java/fr/dauphine/ja/tranyves/iterable/Panel.java
+++ iterable/src/main/java/fr/dauphine/ja/tranyves/iterable/Panel.java
@@ -2,7 +2,30 @@ package fr.dauphine.ja.tranyves.iterable;
 
 import java.util.Iterator;
 
-public class Panel {
+public class Panel implements Iterator<Integer> {
+
+    private int currentValue;
+    private int lastValue;
+
+    public Panel(int firstValue, int lastValue) {
+        this.currentValue = firstValue;
+        this.lastValue = lastValue;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.currentValue <= this.lastValue;
+    }
+
+    @Override
+    public Integer next() {
+        return this.currentValue;
+    }
+
+    @Override
+    public void remove() {
+        // Do nothing
+    }
 
     public static Iterator<Integer> panel1Old(int firstValue, int lastValue) {
         return new PanelIterator(firstValue, lastValue);
@@ -29,6 +52,18 @@ public class Panel {
                 // Do nothing
             }
         };
+
+    }
+
+    public static Iterable<Integer> panel2(final int firstValue, final int lastValue) {
+        return new Iterable<Integer>() {
+
+            @Override
+            public Iterator iterator() {
+                return panel1(firstValue, lastValue);
+            }
+
+        };
     }
 
     public static void main(String [] args) {
@@ -37,6 +72,11 @@ public class Panel {
         Iterator<Integer> it = panel1(1, 5);
         for (; it.hasNext(); ){
             System.out.println(it.next());
+        }
+
+        System.out.println("Start 2");
+        for (int i : panel2(1, 5)) {
+            System.out.println(i);
         }
 
     }
